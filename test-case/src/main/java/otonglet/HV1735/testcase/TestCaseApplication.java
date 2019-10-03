80c8d53cde0819dab2c1abdb16add8ab6c2fb3f8
olivier <olivier.tonglet@emploi.belgique.be>
2019-10-03 10:56:48 +0200

    make the application runnable (nice-to-have)

--- test-case/src/main/java/otonglet/HV1735/testcase/TestCaseApplication.java
+++ test-case/src/main/java/otonglet/HV1735/testcase/TestCaseApplication.java
@@ -2,21 +2,10 @@ package otonglet.HV1735.testcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import otonglet.HV1735.testcase.configuration.NoOpMessageInterpolator;
 
 @SpringBootApplication
 public class TestCaseApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TestCaseApplication.class, args);
-    }
-
-    @Bean
-    LocalValidatorFactoryBean localValidatorFactoryBean() {
-        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
-        factory.setMessageInterpolator(new NoOpMessageInterpolator());
-        return factory;
     }
 }
